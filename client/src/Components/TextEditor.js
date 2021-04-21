8970c9398c36333f82bfc3a90c324bb122a848f9
luffyguy <harshtanwar25@gmail.com>
2021-04-21 13:23:10 +0530

    connected client to the server

--- client/src/Components/TextEditor.js
+++ client/src/Components/TextEditor.js
@@ -1,17 +1,78 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
+import { io } from "socket.io-client";
 
+//toolbar options
+const toolbarOptions = [
+  [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+  [{ font: [] }],
+  ["bold", "italic", "underline"], // toggled buttons
+  ["blockquote", "code-block"],
+  [{ list: "ordered" }, { list: "bullet" }],
+  [{ script: "sub" }, { script: "super" }], // superscript/subscript
+  [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+  [{ direction: "rtl" }], // text direction
+
+  [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+  [{ align: [] }],
+
+  ["clean"], // remove formatting button
+];
 const TextEditor = () => {
+  const [socket, setSocket] = useState();
+  const [quill, setQuill] = useState();
+
+  //to connect to server
+  useEffect(() => {
+    const s = io("http://localhost:3001");
+    setSocket(s);
+    return () => {
+      s.disconnect();
+    };
+  }, []);
+
+  //to send all the changes
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+
+    const handler = (delta, oldDelta, source) => {
+      if (source !== "user") return;
+      socket.emit("send-changes", delta);
+    };
+    quill.on("text-change", handler);
+    return () => {
+      quill.off("text-change", handler);
+    };
+  });
+
+  //to recieve all the changes
+  useEffect(() => {
+    if (socket == null || quill == null) return;
+
+    const handler = (delta) => {
+      quill.updateContents(delta);
+    };
+    socket.on("recieve-changes", handler);
+    return () => {
+      quill.off("recieve-changes", handler);
+    };
+  });
+
+  //to display the text area and update it
   const wrapperRef = useCallback((wrapper) => {
     if (wrapper == null) return;
 
     wrapper.innerHTML = "";
     const editor = document.createElement("div");
     wrapper.append(editor);
-    new Quill(editor, {
+    const q = new Quill(editor, {
+      modules: {
+        toolbar: toolbarOptions,
+      },
       theme: "snow",
     });
+    setQuill(q);
   }, []);
   return <div className="container" ref={wrapperRef}></div>;
 };
