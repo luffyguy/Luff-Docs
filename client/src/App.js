0f733396ee8ed8601f7a1ff7d020a69aad3c511a
luffyguy <harshtanwar25@gmail.com>
2021-04-21 13:37:23 +0530

    fixed routing and provided random id's to each doc

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import TextEditor from "./Components/TextEditor";
-
+import { Switch, Route, Redirect } from "react-router-dom";
+import { v4 as uuidV4 } from "uuid";
 
 function App() {
   return (
-    <div>
-      <TextEditor />
-    </div>
+    <Switch>
+      <Route exact path="/">
+        <Redirect to={`documents/${uuidV4()}`} />
+      </Route>
+      <Route path="/documents/:id">
+        <TextEditor />
+      </Route>
+    </Switch>
   );
 }
 
